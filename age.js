5b13deec9f69a99ae99b2cabd9cd10e5e2d02c31
Artem Kim <artemkimlondon@gmail.com>
2020-08-01 12:20:44 +0100

    Exercises for I/O

--- age.js
+++ age.js
@@ -1,4 +1,6 @@
-let age = 27;
+let rlSync = require('readline-sync');
+let age = Number(rlSync.question("How old are you?\n"));
+console.log(`You are ${age} years old`);
 console.log(`In 10 years, you will be ${age + 10}`);
 console.log(`In 20 years, you will be ${age + 20}`);
 console.log(`In 30 years, you will be ${age + 30}`);
--- greeter.js
+++ greeter.js
@@ -1,4 +1,5 @@
-let name = "Victor";
-console.log(`Good Morning, ${name}`);
-console.log(`Good Afternoon, ${name}`);
-console.log(`Good Evening, ${name}`);
+let rlSync = require('readline-sync');
+let firstName = rlSync.question("What is your first name?\n");
+let lastName = rlSync.question("What is your last name?\n");
+
+console.log(`Hello, ${firstName} ${lastName}!`);
